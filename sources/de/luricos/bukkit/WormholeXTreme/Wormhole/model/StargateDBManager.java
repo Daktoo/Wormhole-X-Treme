bb8a8d606a40e72877951ee7e1dacc59ffa78752
Dakto <dakotakearneycole@outlook.com>
2026-06-28 16:11:32 +0100

    Add import java.sql.Statemate;

--- sources/de/luricos/bukkit/WormholeXTreme/Wormhole/model/StargateDBManager.java
+++ sources/de/luricos/bukkit/WormholeXTreme/Wormhole/model/StargateDBManager.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
